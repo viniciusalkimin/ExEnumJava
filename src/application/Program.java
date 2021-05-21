f5ea5915be9914948e328fa5545c364e0636dd1f
Vinicius Alkimin <viniciusg_alkimin@hotmail.com>
2021-05-21 11:37:50 -0300

    Inicio da class application

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -12,7 +13,8 @@ import entities.enums.WorkerLevel;
 
 public class Program {
 
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws ParseException {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -24,21 +26,19 @@ public class Program {
 
 		System.out.print("Name: ");
 		String workerName = sc.nextLine();
-
 		System.out.print("Level: ");
 		String workerLevel = sc.nextLine();
 		System.out.print("Base salary: ");
 		double baseSalary = sc.nextDouble();
 
-		Worker worker = new Worker(workerName, workerLevel.valueOf(workerLevel), baseSalary,
-				new Department(departmentName));
+		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary , new Department(departmentName));
 
 		System.out.print("How many contracts to this worker? ");
 		int n = sc.nextInt();
 
-		for (int i = 1; i < n; i++) {
-			System.out.println("Enter contract #" + i + "data:");
-			System.out.print("Date: ");
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Enter contract #" + i + " data:");
+			System.out.print("Date (DD/MM/YYYY): ");
 			Date contractDate = sdf.parse(sc.next());
 			System.out.print("Value per hour: ");
 			double valuePerHour = sc.nextDouble();
@@ -46,8 +46,10 @@ public class Program {
 			int hours = sc.nextInt();
 			HourContract contract = new HourContract(contractDate, valuePerHour, hours);
 			worker.addContract(contract);
-		}
+			System.out.println();
+		}	
 
+			System.out.println("total:");
 		sc.close();
 	}
 
--- src/entities/HourContract.java
+++ src/entities/HourContract.java
@@ -46,4 +46,5 @@ public class HourContract {
 		return valuePerHour * hours;
 	}
 
+		
 }
